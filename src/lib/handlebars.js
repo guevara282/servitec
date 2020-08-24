c6ae82e2b0a8506fb18d710e6e5d0376bae4e1d7
Daniel <daniel_andres.03@outlook.com>
2020-08-23 20:52:13 -0500

    refactor: add special condition in hbs

--- src/lib/handlebars.js
+++ src/lib/handlebars.js
@@ -1,7 +1,35 @@
 const timeago = require('timeago.js');
 const timeagoInstance = timeago();
 
-const helpers = {};
+const helpers = {
+    ifCond: function (v1, operator, v2, options) {
+
+        switch (operator) {
+            case '==':
+                return (v1 == v2) ? options.fn(this) : options.inverse(this);
+            case '===':
+                return (v1 === v2) ? options.fn(this) : options.inverse(this);
+            case '!=':
+                return (v1 != v2) ? options.fn(this) : options.inverse(this);
+            case '!==':
+                return (v1 !== v2) ? options.fn(this) : options.inverse(this);
+            case '<':
+                return (v1 < v2) ? options.fn(this) : options.inverse(this);
+            case '<=':
+                return (v1 <= v2) ? options.fn(this) : options.inverse(this);
+            case '>':
+                return (v1 > v2) ? options.fn(this) : options.inverse(this);
+            case '>=':
+                return (v1 >= v2) ? options.fn(this) : options.inverse(this);
+            case '&&':
+                return (v1 && v2) ? options.fn(this) : options.inverse(this);
+            case '||':
+                return (v1 || v2) ? options.fn(this) : options.inverse(this);
+            default:
+                return options.inverse(this);
+        }
+    }
+};
 
 helpers.timeago = (savedTimestamp) => {
     return timeagoInstance.format(savedTimestamp);
